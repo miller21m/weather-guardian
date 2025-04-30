82f1ee3627ed076b5cb4c989ab2dde897ccee527
miller21m <miller05488@gmail.com>
2025-04-30 16:46:49 +0300

    update prod route

--- server/index.js
+++ server/index.js
@@ -20,12 +20,14 @@ app.use('/alert', alertRoute);
 
 // Serve React build (only in production)
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    const clientBuildPath = path.join(__dirname, '../client/build');
+    app.use(express.static(clientBuildPath));
   
+    // Wildcard route for React (v5-compatible)
     app.get('/:path(*)', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+      res.sendFile(path.join(clientBuildPath, 'index.html'));
     });
-  }
+}
 
   const PORT = process.env.PORT || 3000;
 
