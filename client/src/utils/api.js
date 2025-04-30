7d57955b17e0a0618269a33a2c77fc2cc3aed668
miller21m <miller05488@gmail.com>
2025-04-30 15:32:27 +0300

    Adding .env for client

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
 
 export const api = {
   async request(endpoint, method = 'GET', data = null) {
